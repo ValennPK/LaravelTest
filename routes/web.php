4edc83d99b1ee80b2aae989a25467fb4847d18b1
ValennPK <163018284+ValennPK@users.noreply.github.com>
2024-05-27 12:03:33 -0300

    routes modified added blog, contact and about

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 
+/*Route::get('/', function () {
+    return view('welcome');
+});
+*/
 Route::view('/','welcome');
-
-
+Route::view('/contacto','contact');
+Route::view('/blog','blog');
+Route::view('/about','about');

a39023743c39ca08a72006224debd93f9e623f89
ValennPK <163018284+ValennPK@users.noreply.github.com>
2024-06-07 19:39:25 -0300

    auth applied to blogs

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,16 +8,22 @@
  
 
     <h1>Blog</h1>  
-    <a href="{{ route('posts.create')}}">New post</a>
+    @auth
+        <a href="{{ route('posts.create')}}">New post</a>    
+    @endauth
+    
     <ul>
         @foreach ($posts as $post)
             <div style="display: flex; aling-posts: baseline">
                 <li><a href="{{route('posts.show',$post)}}">{{$post->title}}</a></li>&nbsp;
-                <a href="{{route('posts.edit',$post)}}">Edit</a> &nbsp;
-                <form action="{{route('posts.destroy', $post)}}" method="POST">
-                @csrf @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
+                @auth
+                    <a href="{{route('posts.edit',$post)}}">Edit</a> &nbsp;
+                    <form action="{{route('posts.destroy', $post)}}" method="POST">
+                    @csrf @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>                    
+                @endauth
+
             </div>
         @endforeach
     </ul>
